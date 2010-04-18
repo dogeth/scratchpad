cad4f81b7c6c74ce63c0c06d615975a33baf0739
Kevin Read <readkev@gmail.com>
2010-04-18 17:57:58 +1000

    Added journeys

---
+++ translink/journeys.rb
@@ -0,0 +1,24 @@
+#Script to pull journeys from translink
+
+require 'rubygems'
+require 'rest_client'
+require 'nokogiri'
+
+RestClient.log = 'RC.log'
+
+origin = 'CENTRAL+RAILWAY+STATION'
+destination = 'COORPAROO+RAILWAY+STATION'
+journeydate = '19/4/2010'
+
+# response = RestClient.get "http://jp.translink.com.au/TransLinkFromToTimetable.asp?Origin=#{origin}&Destination=#{destination}&Date=#{journeydate}"
+# f = File.open('journeys.html', 'w')
+# f.syswrite(response)
+
+
+doc = Nokogiri::HTML(open('journeys.html'))
+
+doc.css("th.TimetableRouteNumber a").each do |o|
+  if o[:href] =~ /TripKey=(.+)&Date=(.+)&Route=(.+)&Origin=(.+)&Destination=(.+)&ArriveTime=(.+)&DepartTime=(.+)/
+    puts "#{$1} #{$2} #{$3} #{$4} #{$5} #{$6} #{$7}"
+  end
+end
