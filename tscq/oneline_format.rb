f20fe1158faf00a92c647f410f757761e960b865
Kevin Read <readkev@gmail.com>
2011-11-03 06:26:21 +1000

    tscq updates

--- tscq/oneline_format.rb
+++ tscq/oneline_format.rb
@@ -10,6 +10,8 @@ timezone_offset = 10
 
 # Open a file or pass a string to the parser
 cal_file = File.open('TSCQ.ics')
+start_from_date = Date.new(2012,1,1)
+
 
 # Parser returns an array of calendars because a single file
 # can have multiple calendars.
@@ -20,19 +22,24 @@ File.open('TSCQ.txt','w') do |f|
 
   cal.events.sort_by { |e| e.dtstart}.each do |e|
     #Must be a better way to do this... I had to fool around with time zone info.  Also, full day events had a finish time of midnight (needed to bring that back into today)
+    
     dtstart = DateTime.parse(e.dtstart.to_s) 
-    dtend = DateTime.parse(e.dtend.to_s) 
-    dtstart += 10.hours if (dtstart.hour > 0 || dtend.hour > 0)
-    dtend += dtend.hour > 0 ? 10.hours : -1.second
+  
+    if (dtstart > start_from_date)
 
-    first_date = true
-    while Date.parse(dtstart.strftime('%Y/%m/%d')) <= Date.parse(dtend.strftime('%Y/%m/%d'))
-      display_time = (first_date && dtstart.hour > 0) ? "#{dtstart.strftime('%H:%M')}" : "     "
+      dtend = DateTime.parse(e.dtend.to_s) 
+      dtstart += 10.hours if (dtstart.hour > 0 || dtend.hour > 0)
+      dtend += dtend.hour > 0 ? 10.hours : -1.second
 
-      f.puts "#{dtstart.strftime('%d/%m/%Y')} #{display_time} #{e.summary}"
-      dtstart+=1
-      first_date = false
-    end  
+      first_date = true
+      while Date.parse(dtstart.strftime('%Y/%m/%d')) <= Date.parse(dtend.strftime('%Y/%m/%d'))
+        display_time = (first_date && dtstart.hour > 0) ? "#{dtstart.strftime('%H:%M')}" : "     "
+
+        f.puts "#{dtstart.strftime('%d/%m/%Y')} #{display_time} #{e.summary}"
+        dtstart+=1
+        first_date = false
+      end  
+    end
 
   end
  
